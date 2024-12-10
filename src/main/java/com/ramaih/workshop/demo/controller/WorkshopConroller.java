59e84486b01f3dbb87c06d1095ee88a78e485328
Manikanta Bandaru <manikanta.bandaru@gmail.com>
2024-12-10 10:07:39 +0530

    Demo project

---
+++ src/main/java/com/ramaih/workshop/demo/controller/WorkshopConroller.java
@@ -0,0 +1,17 @@
+package com.ramaih.workshop.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WorkshopConroller {
+
+@GetMapping("/workshop")
+public @ResponseBody String getWorkshop() {
+return "Welcome to the workshop";
+}
+
+
+
+}
